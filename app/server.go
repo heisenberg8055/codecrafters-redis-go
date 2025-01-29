a761bde31a12a4a98af486d0512ec7535dbbe343
Yeswanth <yeswanthsai.inturi@gmail.com>
2025-01-29 18:23:31 +0530

    Pong response multiple space

--- app/server.go
+++ app/server.go
@@ -24,7 +24,7 @@ func main() {
 	for {
 		conn, err := l.Accept()
 		if err != nil {
-			fmt.Println("Error accepting connection: ", err.Error())
+			fmt.Println("Error accepting connection:", err.Error())
 			os.Exit(1)
 		}
 		conn.Write([]byte("+PONG\r\n"))
