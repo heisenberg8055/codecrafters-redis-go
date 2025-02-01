966294061b3a6bafd924581997a8a0ab2454b120
Yeswanth <yeswanthsai.inturi@gmail.com>
2025-02-01 10:16:31 +0530

    test

--- app/server.go
+++ app/server.go
@@ -16,8 +16,9 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	conn, err := l.Accept()
+	defer l.Close()
 	for {
+		conn, err := l.Accept()
 		if err != nil {
 			fmt.Println("Error accepting connection: ", err.Error())
 			os.Exit(1)
