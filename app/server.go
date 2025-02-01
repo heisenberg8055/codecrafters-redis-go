f024fcf0cd46b183816d50666045db121323733e
Yeswanth <yeswanthsai.inturi@gmail.com>
2025-02-01 10:19:12 +0530

    test

--- app/server.go
+++ app/server.go
@@ -17,8 +17,9 @@ func main() {
 		os.Exit(1)
 	}
 	defer l.Close()
+	conn, err := l.Accept()
 	for {
-		conn, err := l.Accept()
+
 		if err != nil {
 			fmt.Println("Error accepting connection: ", err.Error())
 			os.Exit(1)
